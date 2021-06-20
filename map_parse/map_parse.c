e8bf196ea37db25b5450fa182317b22e8c70b925
Hyeon yi Nam <hyenam@c7r7s4.42seoul.kr>
2021-06-20 17:22:06 +0900

    Edit check_option

--- map_parse/map_parse.c
+++ map_parse/map_parse.c
@@ -6,7 +6,7 @@
 /*   By: hyenam <hyenam@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 19:43:50 by hyenam            #+#    #+#             */
-/*   Updated: 2021/06/20 13:53:04 by hyenam           ###   ########.fr       */
+/*   Updated: 2021/06/20 17:20:53 by hyenam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		map_parse(int fd, t_game *map)
 	{
 		if (op_num == 6)
 			check_blank(buf, &temp, &blank);
-		if (op_num != 6)
+		else
 			op_num += check_option(map, buf);
 		free(buf);
 	}
@@ -61,16 +61,18 @@ int		check_option(t_game *map, char *buf)
 	{
 		if (buf[0] == 'N' && buf[1] == 'O')
 			pass += set_tex_path(map, buf, 1);
-		if (buf[0] == 'S' && buf[1] == 'O')
+		else if (buf[0] == 'S' && buf[1] == 'O')
 			pass += set_tex_path(map, buf, 2);
-		if (buf[0] == 'W' && buf[1] == 'E')
+		else if (buf[0] == 'W' && buf[1] == 'E')
 			pass += set_tex_path(map, buf, 3);
-		if (buf[0] == 'E' && buf[1] == 'A')
+		else if (buf[0] == 'E' && buf[1] == 'A')
 			pass += set_tex_path(map, buf, 4);
-		if (buf[0] == 'F' && buf[1] == ' ')
+		else if (buf[0] == 'F' && buf[1] == ' ')
 			pass += set_color(map, &buf[1], 1);
-		if (buf[0] == 'C' && buf[1] == ' ')
+		else if (buf[0] == 'C' && buf[1] == ' ')
 			pass += set_color(map, &buf[1], 0);
+		else
+			error_handler("Error\nUnspecified identifier");
 	}
 	else
 		error_handler("Error\nUnspecified identifier");
